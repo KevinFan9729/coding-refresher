31ea438f5fe1a545a66364c8b877a99791ca724e
KevinFan9729 <keveinfan@gmail.com>
2023-09-05 13:59:43 -0400

    collision finding, reversed linear

---
+++ Car Fleet/sol1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        combined = list(zip(position, speed))
+        combined.sort()
+        ans = 0
+        memory = set()
+        R = len(combined)-1
+        if R == 0:
+            return 1
+        L = R-1
+        while R>=0 and L >=0: 
+            right_time = (target - combined[R][0])/combined[R][1]
+            left_time = (target - combined[L][0])/combined[L][1]
+            if right_time >= left_time:#right car is slower
+                if combined[R] not in memory:
+                    ans+=1
+                memory.add(combined[R])
+                L-=1
+            else: #right car is faster, no combined fleet
+                if combined[R] not in memory: # the right car itself form a fleet
+                    ans+=1
+                memory.add(combined[R])
+                R = L
+                if L!=0:
+                    L-=1
+        return ans
