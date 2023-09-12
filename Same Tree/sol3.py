c34357d0deac3d247dfbf3948dbfc328783d1222
KevinFan9729 <keveinfan@gmail.com>
2023-09-12 17:01:52 -0400

    same tree

---
+++ Same Tree/sol3.py
@@ -0,0 +1,20 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None or q==None:
+            if p == q:
+                return True
+            else:
+                return False
+        if p.val == q.val:
+            left_check = self.isSameTree(p.left, q.left)
+            right_check = self.isSameTree(p.right, q.right)
+            return left_check and right_check
+        else:
+            return False
